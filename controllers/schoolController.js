0b27967e6c9566ed0061e3df3113d5b91a6edb0d
NeerajaNeelima <023neeraja@gmail.com>
2024-08-23 14:09:51 +0530

    Online Mysql

--- controllers/schoolController.js
+++ controllers/schoolController.js
@@ -2,16 +2,16 @@ const db = require('../config/db');
 
 
 exports.addSchool = async (req, res) => {
-    const { name, address, lattitude, longitude } = req.body;
+    const { name, address, latitude, longitude } = req.body;
     
-    if (!name || !address || typeof lattitude !== 'number' || typeof longitude !== 'number') {
+    if (!name || !address || typeof latitude !== 'number' || typeof longitude !== 'number') {
         return res.status(400).json({ error: 'Invalid input data' });
     }
 
-    console.log(name, address, lattitude, longitude)
+    console.log(name, address, latitude, longitude)
     try {
-        await db.query('INSERT INTO schoos (name, address, lattitude, longitude) VALUES (?, ?, ?, ?)', 
-            [name, address, lattitude, longitude]);
+        await db.query('INSERT INTO schools (name, address, latitude, longitude) VALUES (?, ?, ?, ?)', 
+            [name, address, latitude, longitude]);
 
         res.status(201).json({ message: 'School added successfully' });
     } catch (error) {
@@ -20,14 +20,14 @@ exports.addSchool = async (req, res) => {
 };
 
 exports.listSchools = async (req, res) => {
-    const { lattitude, longitude } = req.query;
+    const { latitude, longitude } = req.query;
 
     try {
-        const [schoos] = await db.query('SELECT * FROM schoos');
+        const [schools] = await db.query('SELECT * FROM schools');
 
         // Calculate distance and sort by proximity
-        const sortedSchools = schoos.map(school => {
-            const distance = Math.sqrt(Math.pow(lattitude - school.lattitude, 2) + Math.pow(longitude - school.longitude, 2));
+        const sortedSchools = schools.map(school => {
+            const distance = Math.sqrt(Math.pow(latitude - school.latitude, 2) + Math.pow(longitude - school.longitude, 2));
             return { ...school, distance };
         }).sort((a, b) => a.distance - b.distance);
 
